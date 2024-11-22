8c115dcecac9b327ee673b47b7e53c9955be940e
Ilya-23 <ilya.gawrin.7635@gmail.com>
2024-11-22 16:20:27 +0700

    Add files via upload

---
+++ module_9_4.py
@@ -0,0 +1,33 @@
+import random
+"""Lambda-функция:"""
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+result_1 = list(map(lambda x, y: x == y, first, second))
+print(result_1)
+
+"""Замыкание:"""
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        for i in data_set:
+            with open(file_name, 'a', encoding='utf-8') as file:
+                file.write(f'{i}\n')
+    return write_everything
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+"""Метод __call__:"""
+class MysticBall:
+
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        word = random.choice(self.words)
+        return word
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
+
+
